2187ff734e4b4ac5be6e1460ce950e46c7a5c63c
Kevin Hastie <kevin.hastie@proquest.com>
2015-02-27 14:10:47 -0500

    Initial changes to forked apache log parsing code

--- index.js
+++ index.js
@@ -1,184 +1,247 @@
 module.exports = function (line) {
 
-  var parsed = {};
+	var parsed = {
+		'timeLocal' : null,
+		'accountId' : null,
+		'customer' : null,
+		'trueIp' : null,
+		'ip' : null,
+		'jsessionid' : null,
+		'status' : 0,
+		'method' : null,
+		'path' : null,
+		'duration' : 0,
+		'bodyBytesSent' : 0,
+		'userAgent' : null,
+		'protocol' : null
+	};
 
-  //
-  // Trailing newline? NOTHX
-  //
-  if (line.match(/\n$/)) {
-    line = line.slice(0, line.length - 1);
-  }
+	//
+	// Trailing newline? NOTHX
+	//
+	if (line.match(/\n$/)) {
+		line = line.slice(0, line.length - 1);
+	}
 
-  //
-  // Pull out as many fields as possible
-  //
-  [
-    { 'remote_addr'          : ' - ' },
-    { 'remote_user'          : ' ['  },
-    { 'time_local'           : '] "' },
-    { 'request'              : '" '  },
-    { 'status'               : ' '   },
-    { 'body_bytes_sent'      : ' '   },
-    { 'http_referer'         : ' "'  },
-    { 'http_user_agent'      : '" "' },
-    { 'http_x_forwarded_for' : '"'   }
-  ].some(function (t) {
-    var label = Object.keys(t)[0],
-        delimiter = t[label],
-        field;
+	//
+	// Pull out as many fields as possible
+	//
+	[
+		{ 'ip'					: ' "True-Client-IP:'	},
+		{ 'trueIp'				: '" "AppVersion:r' 	},
+		{ 'appVersion'			: '" '  },
+		{ 'skip'				: ' ['  }, // skip remote_logname & remote_user
+		{ 'timeLocal'			: '] "' },
+		{ 'request'             : '" '  },
+		{ 'status'              : ' '   },
+		{ 'bodyBytesSent'      	: ' "'  },
+		{ 'referrer'         	: '" "' },
+		{ 'userAgent'      		: '" '  },
+		{ "duration"    		: ' '   },
+		{ 'skip'			    : ' "'  }, // skip processId
+		{ 'jsessionid'			: '"'   }
+// skip last field: BIGipServer ("-")
 
-    var m = line.match(escape(delimiter));
+	].some(function (t) {
+			var label = Object.keys(t)[0],
+				delimiter = t[label],
+				field;
 
-    if (m === null) {
-      //
-      // No match. Try to pick off the last element.
-      //
-      m = line.match(escape(delimiter.slice(0, 1)));
+			var m = line.match(escape(delimiter));
 
-      if (m === null) {
-        field = line;
-      }
-      else {
-        field = line.substr(0, m.index);
-      }
+			if (m === null) {
+				//
+				// No match. Try to pick off the last element.
+				//
+				m = line.match(escape(delimiter.slice(0, 1)));
 
-      parsed[label] = field;
+				if (m === null) {
+					field = line;
+				}
+				else {
+					field = line.substr(0, m.index);
+				}
 
-      return true;
-    }
- 
-    field = line.substr(0, m.index);
-    line = line.substr(m.index + delimiter.length);
+				parsed[label] = field;
 
-    parsed[label] = field;
-  });
+				return true;
+			}
 
-  var matches;
-  if (parsed.request) {
-    var matches = parsed.request.match(/([A-Z]+)\s+(\S+)\s+([A-Z]+\/[\d\.]+)/);
-    if (matches) {
-      parsed.method = parsed.http_method = matches[1];
-      parsed.path = matches[2];
-      parsed.protocol = matches[3];
-    }
-  }
+			field = line.substr(0, m.index);
+			line = line.substr(m.index + delimiter.length);
 
-  //
-  // So the fields don't choke the implicit RegExp(str) in .match
-  //
-  function escape(d) {
-    return d
-      .replace('[', '\\[')
-      .replace(']', '\\]')
-    ;
-  }
+			parsed[label] = field;
+		});
 
-  //
-  // Deal with placeholders
-  //
-  Object.keys(parsed).forEach(function (k) {
-    if (parsed[k] === '-') {
-      parsed[k] = null;
-    }
-  });
+	if (parsed.request) {
+		var matches = parsed.request.match(/([A-Z]+)\s+(\/professional)+(\S+)\s+([A-Z]+\/[\d\.]+)/);
+		if (matches) {
+			parsed.method = matches[1];
+			parsed.path = matches[3];
+			parsed.protocol = matches[4];
+		}
+	}
 
-  //
-  // Parse the "local time" field into a javascript date object
-  //
-  if (parsed.time_local) {
-    parsed.time_local = (function (str) {
-      var m,
-          month, day, year, hh, mm, ss, offset = {};
+	delete parsed.request; // .request is now redundant
 
-      //
-      // Day
-      //
-      m = str.match('/');
-      day = parseInt(str.substr(0, m.index), 10);
-      str = str.substr(m.index + 1);
+	//
+	// So the fields don't choke the implicit RegExp(str) in .match
+	//
+	function escape(d) {
+		return d
+			.replace('[', '\\[')
+			.replace(']', '\\]')
+			;
+	}
 
-      //
-      // Month
-      //
-      m = str.match('/');
-      month = str.substr(0, m.index);
-      str = str.substr(m.index + 1);
+	//
+	// Deal with placeholders
+	//
+	Object.keys(parsed).forEach(function (k) {
+		if (parsed[k] === '-') {
+			parsed[k] = null;
+		}
+	});
 
-      month = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'
-        .split(' ')
-        .indexOf(month)
-      ;
+	//
+	// Parse the "local time" field into a javascript date object
+	//
+	if (parsed.timeLocal) {
+		parsed.timeLocal = (function (str) {
+			var m,
+				month, day, year, hh, mm, ss, offset = {};
 
-      //
-      // WHAT YEAR
-      //
-      m = str.match(':');
-      year = parseInt(str.substr(0, m.index), 10);
-      str = str.substr(m.index + 1);
+			//
+			// Day
+			//
+			m = str.match('/');
+			day = parseInt(str.substr(0, m.index), 10);
+			str = str.substr(m.index + 1);
 
-      //
-      // Hours
-      //
-      m = str.match(':');
-      hh = parseInt(str.substr(0, m.index), 10);
-      str = str.substr(m.index + 1);
+			//
+			// Month
+			//
+			m = str.match('/');
+			month = str.substr(0, m.index);
+			str = str.substr(m.index + 1);
 
-      //
-      // Minutes
-      //
-      m = str.match(':');
-      mm = parseInt(str.substr(0, m.index), 10);
-      str = str.substr(m.index + 1);
+			month = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'
+				.split(' ')
+				.indexOf(month)
+			;
 
-      //
-      // Seconds
-      //
-      m = str.match(' ');
-      ss = parseInt(str.substr(0, m.index), 10);
-      str = str.substr(m.index + 1);
+			//
+			// WHAT YEAR
+			//
+			m = str.match(':');
+			year = parseInt(str.substr(0, m.index), 10);
+			str = str.substr(m.index + 1);
 
-      //
-      // Time zone offsets
-      //
-      // TZ sign in standard string representations is REVERSED from sign in JavaScript:
-      //
-      //     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getTimezoneOffset
-      //
-      // "Note that this means that the offset is positive if the local timezone is
-      // behind UTC and negative if it is ahead.  For example, if your time zone
-      // is UTC+10 (Australian Eastern Standard Time), -600 will be returned."
-      //
-      // See also:
-      //
-      //      http://stackoverflow.com/questions/1091372/getting-the-clients-timezone-in-javascript/1091399#1091399
-      //
+			//
+			// Hours
+			//
+			m = str.match(':');
+			hh = parseInt(str.substr(0, m.index), 10);
+			str = str.substr(m.index + 1);
 
-      offset.sign = str.substr(0, 1);
-      offset.hh = parseInt(str.substr(1, 2), 10);
-      offset.mm = parseInt(str.substr(3, 2), 10);
+			//
+			// Minutes
+			//
+			m = str.match(':');
+			mm = parseInt(str.substr(0, m.index), 10);
+			str = str.substr(m.index + 1);
 
-      switch (offset.sign) {
-        case '+': offset.sign = -1; break;
-        case '-': offset.sign = +1; break;
-      }
+			//
+			// Seconds
+			//
+			m = str.match(' ');
+			ss = parseInt(str.substr(0, m.index), 10);
+			str = str.substr(m.index + 1);
 
-      hh += offset.sign * offset.hh;
-      mm += offset.sign * offset.mm;
+			//
+			// Time zone offsets
+			//
+			// TZ sign in standard string representations is REVERSED from sign in JavaScript:
+			//
+			//     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getTimezoneOffset
+			//
+			// "Note that this means that the offset is positive if the local timezone is
+			// behind UTC and negative if it is ahead.  For example, if your time zone
+			// is UTC+10 (Australian Eastern Standard Time), -600 will be returned."
+			//
+			// See also:
+			//
+			//      http://stackoverflow.com/questions/1091372/getting-the-clients-timezone-in-javascript/1091399#1091399
+			//
 
-      return new Date(Date.UTC(year, month, day, hh, mm, ss));
+			offset.sign = str.substr(0, 1);
+			offset.hh = parseInt(str.substr(1, 2), 10);
+			offset.mm = parseInt(str.substr(3, 2), 10);
 
-    })(parsed.time_local);
-  }
+			switch (offset.sign) {
+				case '+':
+					offset.sign = -1;
+					break;
+				case '-':
+					offset.sign = +1;
+					break;
+			}
 
-  //
-  // Do some parseInts on known numerical fields
-  //
-  if (parsed.status) {
-    parsed.status = parseInt(parsed.status, 10);
-  }
-  if (parsed.body_bytes_sent) {
-    parsed.body_bytes_sent = parseInt(parsed.body_bytes_sent, 10);
-  }
+			hh += offset.sign * offset.hh;
+			mm += offset.sign * offset.mm;
 
-  return parsed;
+			return new Date(Date.UTC(year, month, day, hh, mm, ss));
+
+		})(parsed.timeLocal);
+	}
+
+	//
+	// Do some parseInts on known numerical fields
+	//
+	if (parsed.status) {
+		parsed.status = parseInt(parsed.status, 10);
+	}
+	if (parsed.bodyBytesSent) {
+		parsed.bodyBytesSent = parseInt(parsed.bodyBytesSent, 10);
+	}
+	if (parsed.duration) {
+		parsed.duration = parseInt(parsed.duration, 10)
+	}
+
+	function determineAccountId () {
+		var matches;
+
+		parsed.accountId = null;
+
+		if (parsed.path) {
+			matches = parsed.path.match(/accountid=([0-9]+)/i);
+
+			if (matches) {
+				parsed.accountId = matches[1];
+			}
+
+		}
+
+		// if no accountId was available on the request, check the referrer
+		if (!parsed.accountId && parsed.referrer) {
+			matches = parsed.referrer.match(/accountid=([0-9]+)/i);
+			if (matches) {
+				parsed.accountId = matches[1];
+			}
+		}
+	}
+
+	determineAccountId();
+
+	delete parsed.skip; // stuff we don't care about
+
+	// delete large fields
+/*
+	delete parsed.referrer;
+	delete parsed.path;
+	delete parsed.userAgent;
+	delete parsed.jsessionid;
+*/
+
+	return parsed;
 };
--- test.js
+++ test.js
@@ -4,64 +4,131 @@ var tap = require('tap');
 
 var parse = require('./index');
 
-tap.test('parsing this example from the apache guys totally works', function (t) {
+tap.test('Parsing an example apache log line (POST).', function (t) {
 
-  var parsed = parse(
-    '127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326'
-  );
+	var parsed = parse('23.216.10.46 "True-Client-IP:68.107.113.217" "AppVersion:r20142.5.3-2" - - [16/Feb/2015:00:00:59 -0500] "POST /professional/results.expandedbasicsearchbox.searchform HTTP/1.1" 302 - "http://search.proquest.com/professional/results/14AF63AF7F82E6D949B/1?accountid=143640" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36" 0 27101 "B11C7741AD4D912824D00709D1329159-m8.t1" "-"');
 
-  t.equal(parsed.remote_addr, '127.0.0.1', 'we have an ip address!');
-  t.equal(parsed.remote_user, 'frank', 'we have a user!');
-  t.type(parsed.time_local, Date, 'we have a date!');
-  t.equal(parsed.request, 'GET /apache_pb.gif HTTP/1.0', 'we have a request!');
-  t.equal(parsed.method, 'GET', 'we have a method!');
-  t.equal(parsed.path, '/apache_pb.gif', 'we have a path!');
-  t.equal(parsed.protocol, 'HTTP/1.0', 'we have a protocol!');
-  t.equal(parsed.http_method, 'GET', 'we have an http method!');
-  t.equal(parsed.status, 200, 'we have a status code!');
-  t.equal(parsed.body_bytes_sent, 2326, 'we have bytes sent!');
+	t.equal(parsed.ip, '23.216.10.46', 'should have an ip address');
+	t.equal(parsed.trueIp, '68.107.113.217', 'should have an ip address');
+	t.equal(parsed.appVersion, '20142.5.3-2', 'should have an app version');
+	//t.equal(parsed.remoteUser, 'frank', 'should have a user');
+	t.type(parsed.timeLocal, Date, 'should have a date');
+	t.equal(parsed.request, undefined, "should drop request from object");
+	t.equal(parsed.method, 'POST', 'should have a method');
+	t.equal(parsed.path, '/results.expandedbasicsearchbox.searchform', 'should have a path');
+	t.equal(parsed.protocol, 'HTTP/1.1', 'should have a protocol');
+	t.equal(parsed.method, 'POST', 'should have an http method');
+	t.equal(parsed.status, 302, 'should have a status code');
+	t.equal(parsed.bodyBytesSent, null, 'should have nothing for bytes sent');
+	t.equal(parsed.referrer, 'http://search.proquest.com/professional/results/14AF63AF7F82E6D949B/1?accountid=143640', 'should have a referer');
+	t.equal(parsed.userAgent, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36', 'should have a user agent');
+	t.equal(parsed.duration, 0, 'should have a request served in');
+	t.equal(parsed.jsessionid, 'B11C7741AD4D912824D00709D1329159-m8.t1', 'should have a jsessionid');
+	t.equal(parsed.accountId, '143640', 'should have an accountid');
 
-  //
-  // Check timezone consistency of parser by constructing a line with the
-  // current local time so we can predict what ought to happen
-  //
-  // Note: Sign in standard string representations is REVERSED from sign in JavaScript:
-  // http://stackoverflow.com/questions/1091372/getting-the-clients-timezone-in-javascript/1091399#1091399
-  //
-  var now = new Date(),
-      months = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' '),
-      sign = now.getTimezoneOffset() > 0 ? '-' : '+',
-      abs = Math.abs(now.getTimezoneOffset()),
-      line;
-  line = util.format(
-    '127.0.0.1 - frank [%s/%s/%s:%s:%s:%s %s%s%s] "GET /apache_pb.gif HTTP/1.0" 200 2326',
-    pad(now.getDate(), 2),
-    months[now.getMonth()],
-    now.getFullYear(),
-    pad(now.getHours(), 2),
-    pad(now.getMinutes(), 2),
-    pad(now.getSeconds(), 2),
-    sign,
-    pad(Math.floor(abs / 60), 2),
-    pad(abs % 60, 2)
-  );
-  parsed = parse(line);
+	//
+	// Check timezone consistency of parser by constructing a line with the current local time so we can predict what ought to happen
+	//
+	// Note: Sign in standard string representations is REVERSED from sign in JavaScript:
+	// http://stackoverflow.com/questions/1091372/getting-the-clients-timezone-in-javascript/1091399#1091399
+	//
+	var now = new Date(),
+		months = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' '),
+		sign = now.getTimezoneOffset() > 0 ? '-' : '+',
+		abs = Math.abs(now.getTimezoneOffset()),
+		line;
+	line = util.format(
+		'23.216.10.46 "True-Client-IP:68.107.113.217" "AppVersion:r20142.5.3-2" - - [%s/%s/%s:%s:%s:%s %s%s%s] "POST /professional/results.expandedbasicsearchbox.searchform HTTP/1.1" 302 - "http://search.proquest.com/professional/results/14AF63AF7F82E6D949B/1?accountid=143640" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36" 0 27101 "B11C7741AD4D912824D00709D1329159-m8.t1" "-"',
+		pad(now.getDate(), 2),
+		months[now.getMonth()],
+		now.getFullYear(),
+		pad(now.getHours(), 2),
+		pad(now.getMinutes(), 2),
+		pad(now.getSeconds(), 2),
+		sign,
+		pad(Math.floor(abs / 60), 2),
+		pad(abs % 60, 2)
+	);
+	parsed = parse(line);
 
-  //
-  // Milliseconds are not represented in the log format, so round them off
-  //
-  t.equal(
-    Math.floor(parsed.time_local.getTime() / 1000.0),
-    Math.floor(now.getTime() / 1000.0),
-    'Dates and timezones are parsed correctly!'
-  );
-  t.end();
+	//
+	// Milliseconds are not represented in the log format, so round them off
+	//
+
+	 t.equal(
+		 Math.floor(parsed.timeLocal.getTime() / 1000.0),
+		 Math.floor(now.getTime() / 1000.0),
+		 'Dates and timezones are parsed correctly!'
+	 );
+
+	t.end();
+});
+
+tap.test('\nParsing an example apache log line (GET).', function (t) {
+
+	var parsed = parse('209.170.118.196 "True-Client-IP:68.107.113.217" "AppVersion:r20142.5.3-2" - - [16/Feb/2015:00:01:00 -0500] "GET /professional/results/14AF644048532808682/1?accountid=143640 HTTP/1.1" 200 28840 "http://search.proquest.com/professional/results/14AF63AF7F82E6D949B/1?accountid=143640" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36" 5 27466 "B11C7741AD4D912824D00709D1329159-m8.t1" "-"');
+
+	t.type(parsed.timeLocal, Date, 'should have a date');
+	t.equal(parsed.method, 'GET', 'should have a method');
+	t.equal(parsed.path, '/results/14AF644048532808682/1?accountid=143640', 'should have a path');
+	t.equal(parsed.protocol, 'HTTP/1.1', 'should have a protocol');
+	t.equal(parsed.method, 'GET', 'should have an http method');
+	t.equal(parsed.status, 200, 'should have a status code');
+	t.equal(parsed.bodyBytesSent, 28840, 'should have bytes sent');
+	t.equal(parsed.duration, 5, 'should have a request served in');
+
+	var now = new Date(),
+		months = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' '),
+		sign = now.getTimezoneOffset() > 0 ? '-' : '+',
+		abs = Math.abs(now.getTimezoneOffset()),
+		line;
+	line = util.format(
+		'23.216.10.46 "True-Client-IP:68.107.113.217" "AppVersion:r20142.5.3-2" - - [%s/%s/%s:%s:%s:%s %s%s%s] "POST /professional/results.expandedbasicsearchbox.searchform HTTP/1.1" 302 - "http://search.proquest.com/professional/results/14AF63AF7F82E6D949B/1?accountid=143640" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36" 0 27101 "B11C7741AD4D912824D00709D1329159-m8.t1" "-"',
+		pad(now.getDate(), 2),
+		months[now.getMonth()],
+		now.getFullYear(),
+		pad(now.getHours(), 2),
+		pad(now.getMinutes(), 2),
+		pad(now.getSeconds(), 2),
+		sign,
+		pad(Math.floor(abs / 60), 2),
+		pad(abs % 60, 2)
+	);
+	parsed = parse(line);
+
+	//
+	// Milliseconds are not represented in the log format, so round them off
+	//
+	 t.equal(
+		 Math.floor(parsed.timeLocal.getTime() / 1000.0),
+		 Math.floor(now.getTime() / 1000.0),
+		 'Dates and timezones should parse correctly'
+	 );
+
+	t.end();
+});
+
+tap.test('\nTesting accountId determination', function(t) {
+
+	var parsed = parse('209.170.118.196 "True-Client-IP:68.107.113.217" "AppVersion:r20142.5.3-2" - - [16/Feb/2015:00:01:00 -0500] "GET /professional/results/14AF644048532808682/1?accountid=1234 HTTP/1.1" 200 28840 "http://search.proquest.com/professional/results/14AF63AF7F82E6D949B/1?accountid=9876" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36" 5 27466 "B11C7741AD4D912824D00709D1329159-m8.t1" "-"'	);
+	t.equal(parsed.accountId, '1234', 'Account ID should be taken from the request when available, instead of referrer.');
+
+	parsed = parse('209.170.118.196 "True-Client-IP:68.107.113.217" "AppVersion:r20142.5.3-2" - - [16/Feb/2015:00:01:00 -0500] "GET /professional/results/14AF644048532808682/1 HTTP/1.1" 200 28840 "http://search.proquest.com/professional/results/14AF63AF7F82E6D949B/1" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36" 5 27466 "B11C7741AD4D912824D00709D1329159-m8.t1" "-"'	);
+	t.equal(parsed.accountId, null, 'Account ID can be null.');
+
+	parsed = parse('209.170.118.196 "True-Client-IP:68.107.113.217" "AppVersion:r20142.5.3-2" - - [16/Feb/2015:00:01:00 -0500] "GET /professional/results/14AF644048532808682/1 HTTP/1.1" 200 28840 "http://search.proquest.com/professional/results/14AF63AF7F82E6D949B/1?accountid=9876" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36" 5 27466 "B11C7741AD4D912824D00709D1329159-m8.t1" "-"'	);
+	t.equal(parsed.accountId, '9876', 'Account ID should be taken from the referrer when it is not found on the request.');
+
+	parsed = parse('209.170.118.196 "True-Client-IP:68.107.113.217" "AppVersion:r20142.5.3-2" - - [16/Feb/2015:00:01:00 -0500] "GET /professional/results/14AF644048532808682/1?AccountID=1234 HTTP/1.1" 200 28840 "http://search.proquest.com/professional/results/14AF63AF7F82E6D949B/1" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36" 5 27466 "B11C7741AD4D912824D00709D1329159-m8.t1" "-"'	);
+	t.equal(parsed.accountId, '1234', 'Account ID should be case insensitive.');
+
+	t.end();
 });
 
 function pad(s, n) {
-  s = s.toString();
-  while (s.length < n) {
-    s = '0' + s;
-  }
-  return s;
+	s = s.toString();
+	while (s.length < n) {
+		s = '0' + s;
+	}
+	return s;
 }
